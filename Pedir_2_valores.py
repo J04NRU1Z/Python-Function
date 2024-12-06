f1d3967f20a8908ac9daa8af82d9291f219871c0
J04NRU1Z <joan.r.r065@gmail.com>
2024-12-06 16:07:22 +0100

    Create Pedir_2_valores.py
    Pide dos valores enteros al usuario.

---
+++ Pedir_2_valores.py
@@ -0,0 +1,14 @@
+def PedirValores():
+    """
+    Pide valores enteros al usuario y los devuelve al programa principal.
+    
+    Args: No necesita
+    
+    Return:
+    (int, int) Valores enteros pedidos al usuario.
+        
+    """
+    a = int(input("Primer valor "))
+    b = int(input("Segundo valor "))
+    return a, b
+
